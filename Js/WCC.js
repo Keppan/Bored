f4a06a600a37c8934c760fb388b3ed9811eae11e
Kevin Östman <kevin.willy@hotmail.com>
2017-03-17 18:05:23 +0100

    Js Added.
    Js Added

---
+++ Js/WCC.js
@@ -0,0 +1,7 @@
+var myArray = ["- Welcome!", "- Välkommen!", "- Bienvenido!", "- Willkommen", "- Bonjour!"];
+var rand = Math.floor(Math.random() * myArray.length);
+
+function showquote(){
+    document.getElementById('p1').textContent = myArray[rand];
+}
+showquote();
